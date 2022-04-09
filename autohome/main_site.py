9b0d8adf98f63d75aac7623d20a904b0fc0f868f
Adriel Igarashi <adriel.igarashi@gmail.com>
2022-04-09 18:46:25 -0300

    arquivo do Docker

--- autohome/main_site.py
+++ autohome/main_site.py
@@ -54,17 +54,17 @@ import streamlit.components.v1 as components
 # components.iframe('https://docs.streamlit.io/en/latest', width=800, height=600)
 
 from streamlit_webrtc import webrtc_streamer
-import av
+# import av
 
 
-class VideoProcessor:
+# class VideoProcessor:
 
-    def recv(self, frame):
-        img = frame.to_ndarray(format="bgr24")
+#     def recv(self, frame):
+#         img = frame.to_ndarray(format="bgr24")
 
-        flipped = img[::-1, :, :]
+#         flipped = img[::-1, :, :]
 
-        return av.VideoFrame.from_ndarray(flipped, format="bgr24")
+#         return av.VideoFrame.from_ndarray(flipped, format="bgr24")
 
 
-webrtc_streamer(key="example", video_processor_factory=VideoProcessor)
+webrtc_streamer(key="example")
