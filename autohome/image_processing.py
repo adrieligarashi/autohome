eaa0c7c91fd46e65418efd3a7b4da23c3c400380
Marcos Tanure <marcostanure@gmail.com>
2022-04-16 13:50:19 -0300

    mudei o age prediction

--- autohome/image_processing.py
+++ autohome/image_processing.py
@@ -4,12 +4,22 @@ from facenet_pytorch import MTCNN
 import cv2
 import numpy as np
 from keras.models import load_model
+from keras.applications.xception import preprocess_input
 
 def get_gender(prob):
     if prob < 0.5:
         return "Male"
     else:
         return "Women"
+
+def get_age(distr):
+    if distr >= 0 and distr < 5:return "Baby"
+    if distr >= 5 and distr < 12:return "Child"
+    if distr >= 12 and distr < 18:return "Teenager"
+    if distr >= 18 and distr < 25:return "Young Adult"
+    if distr >= 25 and distr < 45: return "Adult"
+    if distr >= 45 and distr <= 60: return "Middle Age"
+    return "Old"
 
 pred_passadas = np.array([np.zeros(40) for x in range(0, 7)])
 pred_mean = np.array([0, 0, 0, 0, 0, 0, 0])
@@ -20,7 +30,8 @@ loaded_model = load_model('autohome/models/trained_vggface.h5')
 # loaded_model_gender = load_model('autohome/models/model_gender.h5')
 loaded_model_gender = load_model('autohome/models/gender_test.hdf5')
 loaded_model_ethiny = load_model('autohome/models/model_ethiny.h5')
-loaded_model_age = load_model('autohome/models/model_age.h5')
+# loaded_model_age = load_model('autohome/models/model_age.h5')
+loaded_model_age = load_model('autohome/models/age_prediction.h5')
 
 mtcnn = MTCNN(image_size=240, margin=0, keep_all=True, min_face_size=40)
 
@@ -51,7 +62,8 @@ def image_proc(input):
                 roi = cv2.resize(fc_gray, (48, 48))
                 roi_face = cv2.resize(fc, (96, 96))
                 roi_gender = cv2.resize(fc, (178, 218)) / 255.0
-
+                roi_age_temp = cv2.resize(fc, (224, 224))
+                roi_age = preprocess_input(roi_age_temp)
                 # if n_mean == 5:
                 #     pred = pred_mean
                 #     pred_mean = pred_mean / n_mean
@@ -83,9 +95,9 @@ def image_proc(input):
                 pred_gender = loaded_model_gender.predict(roi_gender[np.newaxis, :, :])
                 pred_ethiny = loaded_model_ethiny.predict(roi[np.newaxis, :, :,
                                                               np.newaxis])
-                pred_age = loaded_model_age.predict(roi[np.newaxis, :, :,
-                                                        np.newaxis])
-
+                # pred_age = loaded_model_age.predict(roi[np.newaxis, :, :,
+                #                                         np.newaxis])
+                pred_age = loaded_model_age.predict(roi_age[np.newaxis, :, :])
 
 
                 text_idx = np.argmax(pred_resume)
@@ -102,10 +114,10 @@ def image_proc(input):
                 text_list_ethiny = [
                     'White', 'Black', 'Asian', 'Indian', 'Others'
                 ]
-                text_idx_age = np.argmax(pred_age)
-                text_list_age = [
-                    'Baby', 'Teen', 'Teenager', 'Young_Adult', 'Adult', 'Old'
-                ]
+                # text_idx_age = np.argmax(pred_age)
+                # text_list_age = [
+                #     'Baby', 'Teen', 'Teenager', 'Young_Adult', 'Adult', 'Old'
+                # ]
 
                 if text_idx == 0:
                     text = text_list[0]
@@ -140,18 +152,21 @@ def image_proc(input):
                 elif text_idx_ethiny == 4:
                     text_ethiny = text_list_ethiny[4]
 
-                if text_idx_age == 0:
-                    text_age = text_list_age[0]
-                if text_idx_age == 1:
-                    text_age = text_list_age[1]
-                elif text_idx_age == 2:
-                    text_age = text_list_age[2]
-                elif text_idx_age == 3:
-                    text_age = text_list_age[3]
-                elif text_idx_age == 4:
-                    text_age = text_list_age[4]
-                elif text_idx_age == 5:
-                    text_age = text_list_age[5]
+                # if text_idx_age == 0:
+                #     text_age = text_list_age[0]
+                # if text_idx_age == 1:
+                #     text_age = text_list_age[1]
+                # elif text_idx_age == 2:
+                #     text_age = text_list_age[2]
+                # elif text_idx_age == 3:
+                #     text_age = text_list_age[3]
+                # elif text_idx_age == 4:
+                #     text_age = text_list_age[4]
+                # elif text_idx_age == 5:
+                #     text_age = text_list_age[5]
+
+                text_age = get_age(pred_age[0])
+
                 box = boxes[i]
                 box = box.astype(int)
 
