653abe623256628a01b2e0652a5fd9c664dfe9de
Vitor Neves <vitor.sneves@hotmail.com>
2022-04-20 16:42:25 -0300

    autoplay funcionando

--- autohome/teste.py
+++ autohome/teste.py
@@ -1,15 +1,12 @@
 from flask import Flask, render_template
-from flask_cors import CORS, cross_origin
 from autohome.music_player import MusicPlayer
 
 app = Flask(__name__)
-CORS(app, origins=["http://127.0.0.1:5000/"])
 
-mood = 'happy'
+mood = 'angry'
 
 sp = MusicPlayer(mood=mood)
 uri, url = sp.create_custom_playlist()
-id = sp.get_device_id()
 token = sp.auth.get_cached_token()['access_token']
 
 
@@ -19,6 +16,5 @@ def index():
     return render_template(
         'apiSpotify.html',
         playlist=uri,
-        token=token,
-        id=id
+        token=token
     )
